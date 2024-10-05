25690b099f9ae27a0d9267303b384053c20d609f
Jakub Leśkiewicz <kubaxl222@gmail.com>
2024-10-05 02:33:12 +0200

    Remove useless endpoints

--- src/routes/training.ts
+++ src/routes/training.ts
@@ -1,9 +1,5 @@
-const trainingController = require('../controllers/trainingController')
 import Router from "./configRouter"
 import { addTraining } from "../controllers/trainingController"
 Router.post('/:id/addTraining',addTraining)
-// Router.get('/:id/getInfoAboutRankAndElo',trainingController.getInfoAboutRankAndElo)
-// Router.post("/:id/getTrainingDates",trainingController.getTrainingDates)
-// Router.get("/getBestTenUsersFromElo",trainingController.getBestTenUsersFromElo)
 
 module.exports = Router
