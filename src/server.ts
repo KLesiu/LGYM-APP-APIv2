fc461c4b0566f7e9dac1583e4f411efb7ba6341a
Lesiuuu <j.leskiewicz@tdz.com.pl>
2025-06-18 15:25:11 +0200

    modify server listen

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-const app = require('./index');
-require("dotenv").config()
-let server; 
+const app = require("./index");
+require("dotenv").config();
+let server;
 
-server=app.listen(process.env.PORT || 4000);
-module.exports = server
+server = app.listen(process.env.PORT || 4000, "0.0.0.0");
 
+module.exports = server;
