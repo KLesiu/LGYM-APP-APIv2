0551d9df42bc680ca75a3eea14f01f41b310382b
Jakub Leśkiewicz <kubaxl222@gmail.com>
2025-06-19 01:07:45 +0200

    change routing

--- dist/helpers/DatesHelpers.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.compareDates = void 0;
-const compareDates = (firstDate, secondDate) => (firstDate.getFullYear() === secondDate.getFullYear() &&
-    firstDate.getMonth() === secondDate.getMonth() &&
-    firstDate.getDate() === secondDate.getDate());
-exports.compareDates = compareDates;
